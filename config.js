c4352fb07ede1d68f5473f62bc45d924e4207270
Ayanshu <ayanshuk22@gmail.com>
2020-12-22 15:39:18 +0530

    Not my first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // API reference URL
+    apiUrl: "https://eu137.chat-api.com/instance208430/",
+    // API token from your personal cabinet
+    token: "cn8enib6l66t0a7e"
+}
